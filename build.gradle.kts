20a7081da45868177fa8729d6f185594b3c4c1fe
Duncan Proctor <duncanproctor@Duncans-MacBook-Pro.local>
2022-01-17 23:58:03 -0500

    changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,6 @@ dependencies {
     implementation("commons-cli:commons-cli:1.5.0")
     implementation("org.spigotmc:spigot-api:1.18.1-R0.1-SNAPSHOT")
     implementation("com.google.inject:guice:5.0.1")
-    implementation(files("../server/bundler/libraries/spigot-api-1.18.1-R0.1-SNAPSHOT.jar"))
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 }
