4cd7101150a72dd7a88e725b5b149a2a666f4671
Duncan Proctor <duncanproctor@Duncans-MacBook-Pro.local>
2022-01-21 13:12:55 -0500

    pass through scope

--- src/main/java/com/duncpro/bukkit/concurrency/Coroutines.kt
+++ src/main/java/com/duncpro/bukkit/concurrency/Coroutines.kt
@@ -11,9 +11,9 @@ import java.util.concurrent.CompletableFuture
  * The coroutine is executed within the [CoroutineScope] of the [JavaPlugin] which provided the class which called
  * this method.
  */
-inline fun <reified T, R> T.future(crossinline body: suspend () -> R): CompletableFuture<R> {
+inline fun <reified T, R> T.future(crossinline body: suspend CoroutineScope.() -> R): CompletableFuture<R> {
     val plugin = JavaPlugin.getProvidingPlugin(T::class.java)
     if (plugin !is IocJavaPlugin) throw IllegalStateException("Only supported for plugins which extend IocJavaPlugin.")
     val pluginScope = plugin.injector.getInstance(PluginCoroutineService::class.java).pluginCoroutineScope
-    return pluginScope.future { body.invoke() }
+    return pluginScope.future { body.invoke(this) }
 }
